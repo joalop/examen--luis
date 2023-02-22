6ce3913b2bba298e30282b894196ee3bdabd8178
Joan <joan1998bocairent@gmail.com>
2023-02-22 14:37:43 +0100

    icons likes and dislikes added

--- database/db.sql
+++ database/db.sql
@@ -6,8 +6,18 @@ create table foto(
 id int primary key auto_increment,
 titulo varchar(50),
 descripcion varchar(100),
-url varchar(100),
-fecha varchar(50),
+url varchar(300),
+fecha varchar(100),
 megusta int,
 nomegusta int);
 
+insert into foto (id, titulo, descripcion, url, fecha, megusta, nomegusta)
+values(
+null,
+'Indiana Jones',
+'Indiana Jones y la nueva cruzada',
+'https://jorgecine1997.files.wordpress.com/2020/11/mv5bzdiznzm5mdutzmi5mc00ngq5lwflnzetyze3odixndi3zmnhxkeyxkfqcgdeqxvynjq4ode4mzq40._v1_.jpg',
+'2023-02-22T010:22:06.761Z (hora estándar de Europa central)',
+0,
+0
+);
